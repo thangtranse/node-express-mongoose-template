8c0cde0e6c22c87907ea7a252a6fe2d0fe9d625e
Thangtranse <thangtran.se@gmail.com>
2022-08-10 14:03:03 +0700

    package ioredis and add method connection

--- datasources/connection.redis.js
+++ datasources/connection.redis.js
@@ -1,12 +1,24 @@
-const redis = require("redis");
+const redis = require("ioredis");
 
-const client = redis.createClient({
+const client = new redis({
   port: process.env.REDIS_PORT || 6379,
   host: process.env.REDIS_HOST || "localhost",
 });
 
 client.ping((err, pong) => {
-  console.log(`redis ping -> pong:: ${pong}`);
+  console.log(`Redis- ping -> pong:: ${pong}`);
+});
+
+client.on("error", function (error) {
+  console.log(`Redis is error:: ${error}`);
+});
+
+client.on("connected", function () {
+  console.log(`Redis is connected`);
+});
+
+client.on("ready", function (data) {
+  console.log(`Redis is ready`);
 });
 
 module.exports = client;
