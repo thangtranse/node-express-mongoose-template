20377bd6d4aa18cb03f16ab87bed2f567d8bd58e
Thangtranse <thangtran.se@gmail.com>
2022-09-09 11:07:49 +0700

    fix: bug path log

--- src/helpers/logs_events.js
+++ src/helpers/logs_events.js
@@ -2,7 +2,26 @@ const fs = require("fs").promises;
 const path = require("path");
 const { format } = require("date-fns");
 
-const FILE_NAME = path.join(__dirname, "../../logs", "log.log");
+const FILE_PATH_LOG = path.join(__dirname, "../../logs");
+
+if (!fs.existsSync(FILE_PATH_LOG)) {
+  fs.mkdirSync(FILE_PATH_LOG);
+}
+
+const FILE_PATH_NAME = path.join(__dirname, "../../logs", "log.log");
+
+if (!fs.existsSync(FILE_PATH_NAME)) {
+  fs.appendFile(
+    FILE_PATH_NAME,
+    `CREATE:: ${new Date().toISOString()}`,
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Create File Log!!!");
+    }
+  );
+}
 
 const logEvents = async ({ url, method, headers, body, msg, ...props }) => {
   const dateLog = `${format(new Date(), `dd-mm-yyyy\thh:mm:ss`)}`;
@@ -15,7 +34,7 @@ const logEvents = async ({ url, method, headers, body, msg, ...props }) => {
     ...props,
   })}\n`;
   try {
-    fs.appendFile(FILE_NAME, contentLog);
+    fs.appendFile(FILE_PATH_NAME, contentLog);
   } catch (error) {
     console.log(`Error:: ${error}`);
   }
