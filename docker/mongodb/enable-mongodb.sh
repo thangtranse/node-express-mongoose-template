e75d9b43c5b783f8184a65d8575dce84a72790ec
Tran Minh Thang | thangtran.se <thangtran.se@gmail.com>
2023-10-05 10:39:30 +0700

    Update

--- docker/mongodb/enable-mongodb.sh
+++ docker/mongodb/enable-mongodb.sh
@@ -1,6 +1,7 @@
 #
 # https://github.com/thangtranse/mongo
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
 #
 
 # enable cluster mongodb
-docker-compose up --detach --build
+docker compose up --detach --build
--- docker/mongodb/stop-mongodb.sh
+++ docker/mongodb/stop-mongodb.sh
@@ -1,6 +1,7 @@
 #
 # https://github.com/thangtranse/mongo
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
 #
 
 # remove docker mongodb images
-docker-compose down
+docker compose down
