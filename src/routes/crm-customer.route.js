379e415997bda599eecf854a65cee151186cf2e8
Tran Minh Thang | thangtran.se <thangtran.se@gmail.com>
2023-11-03 09:43:28 +0700

    Update controller name

--- src/routes/crm-customer.route.js
+++ src/routes/crm-customer.route.js
@@ -5,13 +5,13 @@ const route = express.Router();
 // const { verifyAccessToken } = require("../helpers/jwt_service");
 const crmCustomerController = require("../controllers/crm-customer.controller");
 
-route.post("/register", crmCustomerController.register);
+route.post("/income", crmCustomerController.register);
 
 route.get("/list", crmCustomerController.list);
 
 route.delete("/", crmCustomerController.deleteById);
 
-route.delete("/trash", crmCustomerController.deleteByIds);
+route.delete("/move-to-trash", crmCustomerController.deleteByIds);
 
 route.get("/", crmCustomerController.getDataById);
 
